51f1bdac0a38543a035a60ca36f912cae795eac3
efecollins <efejamb@gmail.com>
2022-09-07 20:48:24 +0100

    Ready for deployment

--- app.js
+++ app.js
@@ -63,6 +63,6 @@ app.get("/posts/:postTitle", (req, res) => {
 
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
